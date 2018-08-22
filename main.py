cdf0c1b6c8600a2dfc0c7b5acd47f1b57aa0347a
Andrey Mikhailov <mikhailov@icc.ru>
2018-08-23 00:41:24 +0800

    Add Pascal VOC file generator

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sys
 import os
 import fnmatch
 import getopt
+from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import SubElement
 import xml.etree.ElementTree as ET
 from struct import unpack
 import binascii
+import struct
 
 
 def main(argv):
@@ -17,7 +21,7 @@ def main(argv):
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('test.py -i <inputfile> -o <outputfile>')
+            print('test.py -i <input folder> -o <output folder>')
             sys.exit()
         elif opt in ("-i", "--i"):
             input_folder = arg
@@ -27,30 +31,69 @@ def main(argv):
     is_dir = os.path.isdir(input_folder)
 
     if not is_dir:
-        raise Exception(input_folder, "- It is a folder")
+        raise Exception(input_folder, "- It is not a folder")
+
+    images = dict()
+
+    for dir_name, sub_dir_list, file_list in os.walk(input_folder):
+        for file_name in fnmatch.filter(file_list, "*.bmp"):
+            path = os.path.join(dir_name, file_name)
+            bmp = open(path, 'rb')
+            bmp.seek(18)
+            width = struct.unpack("I", bmp.read(4))[0]
+            height = struct.unpack("I", bmp.read(4))[0]
+            name = os.path.splitext(os.path.split(path)[1])[0]
+            images[name] = {"width": width, "height": height}
 
     for dir_name, sub_dir_list, file_list in os.walk(input_folder):
         for file_name in fnmatch.filter(file_list, "*.xml"):
             path = os.path.join(dir_name, file_name)
-            process_file(path)
+            name = os.path.splitext(os.path.split(path)[1])[0]
+            if images.get(name) is None:
+                continue
+            pascal_voc = process_file(path, images)
+            pascal_voc_name = os.path.join(output_folder, file_name)
+            print(pascal_voc_name)
+            pascal_voc.write(open(pascal_voc_name, 'w+'), encoding='unicode')
 
-    print('Input file is "', input_folder)
-    print('Output file is "', output_folder)
 
-
-def process_file(path):
+def process_file(path, images):
+    annotation = ET.Element("annotation")
+    tree = ElementTree(annotation)
+    filename = ET.SubElement(annotation, "filename")
+    name = os.path.splitext(os.path.split(path)[1])[0]
+    filename.text = name + ".bmp"
+    size = ET.SubElement(annotation, "size")
+    width = ET.SubElement(size, "width")
+    width.text = str(images[name].get("width"))
+    height = ET.SubElement(size, "height")
+    height.text = str(images[name].get("height"))
+    depth = ET.SubElement(size, "depth")
+    depth.text = str(0)
+    segmented = ET.SubElement(annotation, "segmented")
+    segmented.text = str(0)
     marmot_tree = ET.parse(path)
     marmot_root = marmot_tree.getroot()
-    annotation = ET.Element('annotation')
-    filename = ET.SubElement(annotation, 'filename')
-    base = os.path.splitext(os.path.split(path)[1])[0] + ""
-    filename.text = base + ".bmp"
-    ET.dump(annotation)
-    for table in marmot_root.findall("*/Composites/*[@Label='Table']"):
-        hexs = table.get("BBox").split(" ")
-        bbox = [hex_to_double(x) for x in hexs]
-        print(bbox)
 
+    for table in marmot_root.findall("*/Composites/*[@Label='Table']"):
+        obj = ET.SubElement(annotation, "object")
+        obj_name = ET.SubElement(obj, "name")
+        obj_name.text = "table"
+        bndbox = ET.SubElement(obj, "bndbox")
+
+        hexs = table.get("BBox").split(" ")
+        bbox_array = [hex_to_double(x) for x in hexs]
+
+        xmin = ET.SubElement(bndbox, "xmin")
+        xmin.text = str(bbox_array[0])
+        ymin = ET.SubElement(bndbox, "ymin")
+        ymin.text = str(bbox_array[3])
+        xmax = ET.SubElement(bndbox, "xmax")
+        xmax.text = str(bbox_array[2])
+        ymax = ET.SubElement(bndbox, "ymax")
+        ymax.text = str(bbox_array[1])
+
+    return tree
 
 def hex_to_double(s):
     return unpack(">d", binascii.unhexlify(s))[0]
