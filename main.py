10ca180e4db2c78e618ae8bbde730548b0abc9d3
Andrey Mikhailov <mikhailov@icc.ru>
2018-08-29 15:53:35 +0800

    Add bmp to jpeg coverter

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import os
 import fnmatch
 import getopt
 from xml.etree.ElementTree import ElementTree
-from xml.etree.ElementTree import Element
-from xml.etree.ElementTree import SubElement
 import xml.etree.ElementTree as ET
 from struct import unpack
 import binascii
 import struct
+import shutil
+from PIL import Image
 
 
 def main(argv):
@@ -33,19 +33,34 @@ def main(argv):
     if not is_dir:
         raise Exception(input_folder, "- It is not a folder")
 
+    annotations_path = os.path.join(output_folder, "annotations")
+    xmls_path = os.path.join(annotations_path, "xmlns")
+    images_path = os.path.join(output_folder, "images")
+
+    if os.path.exists(annotations_path):
+        shutil.rmtree(annotations_path)
+
+    os.makedirs(annotations_path)
+    os.makedirs(xmls_path)
+
     images = dict()
 
     for dir_name, sub_dir_list, file_list in os.walk(input_folder):
         for file_name in fnmatch.filter(file_list, "*.bmp"):
             path = os.path.join(dir_name, file_name)
-            bmp = open(path, 'rb')
-            bmp.seek(18)
-            width = struct.unpack("I", bmp.read(4))[0]
-            height = struct.unpack("I", bmp.read(4))[0]
             name = os.path.splitext(os.path.split(path)[1])[0]
-            images[name] = {"width": width, "height": height}
+            save_path = os.path.join(images_path, name + ".jpeg")
+            with Image.open(path) as img:
+                width, height = img.size
+                img.save(save_path)
+            #with open(path, 'rb') as bmp:
+            #    bmp.seek(18)
+            #    width = struct.unpack("I", bmp.read(4))[0]
+            #    height = struct.unpack("I", bmp.read(4))[0]
+            #    name = os.path.splitext(os.path.split(path)[1])[0]
+                images[name] = {"width": width, "height": height}
 
-    trainval_file = open(os.path.join(output_folder, "trainval.txt"), 'w')
+    trainval_file = open(os.path.join(annotations_path, "trainval.txt"), 'w')
 
     for dir_name, sub_dir_list, file_list in os.walk(input_folder):
         for file_name in fnmatch.filter(file_list, "*.xml"):
@@ -58,10 +73,10 @@ def main(argv):
             if pascal_voc is None:
                 continue
 
+            pascal_voc_name = os.path.join(xmls_path, file_name)
             print(pascal_voc_name)
-            pascal_voc_name = os.path.join(output_folder, "xmls", file_name)
             pascal_voc.write(open(pascal_voc_name, 'w'), encoding='unicode')
-            trainval_file.write(name+"\n")
+            trainval_file.write(name + "\n")
 
 
 def process_file(path, images):
@@ -69,14 +84,14 @@ def process_file(path, images):
     tree = ElementTree(annotation)
     filename = ET.SubElement(annotation, "filename")
     name = os.path.splitext(os.path.split(path)[1])[0]
-    filename.text = name + ".bmp"
+    filename.text = name + ".jpeg"
     size = ET.SubElement(annotation, "size")
     width = ET.SubElement(size, "width")
     width.text = str(images[name].get("width"))
     height = ET.SubElement(size, "height")
     height.text = str(images[name].get("height"))
     depth = ET.SubElement(size, "depth")
-    depth.text = str(0)
+    depth.text = str(3)
     segmented = ET.SubElement(annotation, "segmented")
     segmented.text = str(0)
     marmot_tree = ET.parse(path)
@@ -86,15 +101,13 @@ def process_file(path, images):
     if not tables:
         return None
 
-    for table in marmot_root.findall("*/Composites/*[@Label='Table']"):
+    for table in tables:
         obj = ET.SubElement(annotation, "object")
         obj_name = ET.SubElement(obj, "name")
         obj_name.text = "table"
         bndbox = ET.SubElement(obj, "bndbox")
-
         hexs = table.get("BBox").split(" ")
         bbox_array = [hex_to_double(x) for x in hexs]
-
         xmin = ET.SubElement(bndbox, "xmin")
         xmin.text = str(bbox_array[0])
         ymin = ET.SubElement(bndbox, "ymin")
@@ -113,4 +126,3 @@ def hex_to_double(s):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
